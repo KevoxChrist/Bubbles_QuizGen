41a1414a6fa593889729be3214b37974a9aa942a
Thanh Nay <218096274+tin10n@users.noreply.github.com>
2025-10-23 17:56:31 -0400

    made clickable routes

---
+++ src/pages/Quiz.jsx
@@ -0,0 +1,13 @@
+// src/pages/Quiz.jsx
+import React from 'react';
+
+function Quiz() {
+  return (
+    <div className="quiz-page">
+      <h1>Quiz Page</h1>
+      <p>This is just a test page for the NavBar and Footer.</p>
+    </div>
+  );
+}
+
+export default Quiz;
